86292a37696742e752b55ddd2739b0e317a150fb
Brett Fleming <brettfleming015@gmail.com>
2021-08-04 17:29:57 -0500

    added the models for the workouts

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+// const db = require("./models");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bank", { useNewUrlParser: true });
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+  })
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/stats.html"));
+  })
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
